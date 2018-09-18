0299c95696d6cab75dd2c3b1e70a191005909cd5
Drohano <joix99@live.se>
2018-09-18 16:07:17 +0200

    case a, carddeck

--- kortlek21/kortlek21/Program.cs
+++ kortlek21/kortlek21/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace kortlek21
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
+           
             int start = 1;
             do
             {
@@ -25,6 +27,90 @@ namespace kortlek21
                 switch (char.ToLower(ch))
                 {
                     case 'a':
+
+                        //var cards = new String[52];
+
+                        List<string> cardDeck = new List<string>
+                        {
+                            "S_A",
+                            "S_2",
+                            "S_3",
+                            "S_4",
+                            "S_5",
+                            "S_6",
+                            "S_7",
+                            "S_8",
+                            "S_9",
+                            "S_10",
+                            "S_J",
+                            "S_Q",
+                            "S_K",
+                            "H_A",
+                            "H_2",
+                            "H_3",
+                            "H_4",
+                            "H_5",
+                            "H_6",
+                            "H_7",
+                            "H_8",
+                            "H_9",
+                            "H_10",
+                            "H_J",
+                            "H_Q",
+                            "H_K",
+                            "C_A",
+                            "C_2",
+                            "C_3",
+                            "C_4",
+                            "C_5",
+                            "C_6",
+                            "C_7",
+                            "C_8",
+                            "C_9",
+                            "C_10",
+                            "C_J",
+                            "C_Q",
+                            "C_K",
+                            "D_A",
+                            "D_2",
+                            "D_3",
+                            "D_4",
+                            "D_5",
+                            "D_6",
+                            "D_7",
+                            "D_8",
+                            "D_9",
+                            "D_10",
+                            "D_J",
+                            "D_Q",
+                            "D_K"
+                        };
+
+
+                        //public static void Shuffle<T>(this IList<T> list)
+                        
+                        RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+                        int n = cardDeck.Count;
+                        while (n > 1)
+                        {
+                            byte[] box = new byte[1];
+                            do provider.GetBytes(box);
+                            while (!(box[0] < n * (Byte.MaxValue / n)));
+                            int k = (box[0] % n);
+                            n--;
+                            String value = cardDeck[k];
+                            cardDeck[k] = cardDeck[n];
+                            cardDeck[n] = value;
+                        }
+                        
+                        foreach(var card in cardDeck)
+                        {
+                            
+                            Console.WriteLine(card + i);
+                        
+                        }
+
+
 
 
                         start = 1;
