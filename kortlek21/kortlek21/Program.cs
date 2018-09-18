b70b5b95165da3a4455ec8c2bb48e37479af0ff8
Drohano <joix99@live.se>
2018-09-18 14:44:06 +0200

    do + switch

--- kortlek21/kortlek21/Program.cs
+++ kortlek21/kortlek21/Program.cs
@@ -10,6 +10,51 @@ namespace kortlek21
     {
         static void Main(string[] args)
         {
+
+            int start = 1;
+            do
+            {
+
+
+
+
+                char ch;
+                Console.WriteLine("Select a Program \n A , B , C \n Press Q then enter to exit");
+                ch = Convert.ToChar(Console.ReadLine());
+
+                switch (char.ToLower(ch))
+                {
+                    case 'a':
+
+
+                        start = 1;
+                        break;
+
+                    case 'b':
+
+                        start = 1;
+                        break;
+
+                    case 'c':
+
+
+                        start = 1;
+                        break;
+
+                    case 'q':
+                        start = 0;
+                        break;
+
+                    default:
+
+                        start = 1;
+                        break;
+
+                }
+
+            }
+            while (start != 0);
+
         }
     }
 }
