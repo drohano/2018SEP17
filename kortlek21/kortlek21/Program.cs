c4d8af2474685b53d0c1742a8026839250da7d4b
Drohano <joix99@live.se>
2018-09-18 16:19:15 +0200

    minor estetic changes

--- kortlek21/kortlek21/Program.cs
+++ kortlek21/kortlek21/Program.cs
@@ -17,77 +17,76 @@ namespace kortlek21
             do
             {
 
-
-
+                //Switch case meny.
 
                 char ch;
-                Console.WriteLine("Select a Program \n A , B , C \n Press Q then enter to exit");
+                Console.WriteLine("\nSelect a Program \n A , B , C \nPress Q then enter to exit");
                 ch = Convert.ToChar(Console.ReadLine());
 
                 switch (char.ToLower(ch))
                 {
                     case 'a':
 
-                        //var cards = new String[52];
+                        //Creates a Deck of cards.
 
                         List<string> cardDeck = new List<string>
                         {
-                            "S_A",
-                            "S_2",
-                            "S_3",
-                            "S_4",
-                            "S_5",
-                            "S_6",
-                            "S_7",
-                            "S_8",
-                            "S_9",
-                            "S_10",
-                            "S_J",
-                            "S_Q",
-                            "S_K",
-                            "H_A",
-                            "H_2",
-                            "H_3",
-                            "H_4",
-                            "H_5",
-                            "H_6",
-                            "H_7",
-                            "H_8",
-                            "H_9",
-                            "H_10",
-                            "H_J",
-                            "H_Q",
-                            "H_K",
-                            "C_A",
-                            "C_2",
-                            "C_3",
-                            "C_4",
-                            "C_5",
-                            "C_6",
-                            "C_7",
-                            "C_8",
-                            "C_9",
-                            "C_10",
-                            "C_J",
-                            "C_Q",
-                            "C_K",
-                            "D_A",
-                            "D_2",
-                            "D_3",
-                            "D_4",
-                            "D_5",
-                            "D_6",
-                            "D_7",
-                            "D_8",
-                            "D_9",
-                            "D_10",
-                            "D_J",
-                            "D_Q",
-                            "D_K"
+                            "[S_A]",
+                            "[S_2]",
+                            "[S_3]",
+                            "[S_4]",
+                            "[S_5]",
+                            "[S_6]",
+                            "[S_7]",
+                            "[S_8]",
+                            "[S_9]",
+                            "[S_10]",
+                            "[S_J]",
+                            "[S_Q]",
+                            "[S_K]",
+                            "[H_A]",
+                            "[H_2]",
+                            "[H_3]",
+                            "[H_4]",
+                            "[H_5]",
+                            "[H_6]",
+                            "[H_7]",
+                            "[H_8]",
+                            "[H_9]",
+                            "[H_10]",
+                            "[H_J]",
+                            "[H_Q]",
+                            "[H_K]",
+                            "[C_A]",
+                            "[C_2]",
+                            "[C_3]",
+                            "[C_4]",
+                            "[C_5]",
+                            "[C_6]",
+                            "[C_7]",
+                            "[C_8]",
+                            "[C_9]",
+                            "[C_10]",
+                            "[C_J]",
+                            "[C_Q]",
+                            "[C_K]",
+                            "[D_A]",
+                            "[D_2]",
+                            "[D_3]",
+                            "[D_4]",
+                            "[D_5]",
+                            "[D_6]",
+                            "[D_7]",
+                            "[D_8]",
+                            "[D_9]",
+                            "[D_10]",
+                            "[D_J]",
+                            "[D_Q]",
+                            "[D_K]"
                         };
 
 
-                        //public static void Shuffle<T>(this IList<T> list)
+                        //Shuffles the list.
                         
                         RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
                         int n = cardDeck.Count;
@@ -102,11 +101,12 @@ namespace kortlek21
                             cardDeck[k] = cardDeck[n];
                             cardDeck[n] = value;
                         }
-                        
+                        //Prints out the carddeck.
+
                         foreach(var card in cardDeck)
                         {
                             
-                            Console.WriteLine(card + i);
+                            Console.WriteLine(card);
                         
                         }
 
@@ -132,7 +132,7 @@ namespace kortlek21
                         break;
 
                     default:
-
+                        Console.WriteLine("\nTry again\n");
                         start = 1;
                         break;
 
