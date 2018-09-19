845e6be9bd58451eba00e8e72e81d05226ce29cd
Drohano <joix99@live.se>
2018-09-19 13:38:45 +0200

    case c , 21, dont work perfect but works

--- kortlek21/kortlek21/Program.cs
+++ kortlek21/kortlek21/Program.cs
@@ -12,28 +12,10 @@ namespace kortlek21
 
         static void Main(string[] args)
         {
-           
-            int start = 1;
-            do
+            //Creates a Deck of cards.
+
+            IList<Card> cardDeck = new List<Card>()
             {
-
-                //Switch case meny.
-
-                char ch;
-                Console.WriteLine("\nSelect a Program \n A , B , C \nPress Q then enter to exit");
-                ch = Convert.ToChar(Console.ReadLine());
-
-                switch (char.ToLower(ch))
-                {
-                    case 'a':
-
-
-                        Console.WriteLine("\nHere is your deck in a random order\n");
-
-                        //Creates a Deck of cards.
-
-                        IList<Card> cardDeck = new List<Card>()
-                        {
                            new Card(){ number = 1, colour = "H_A" },
                            new Card(){ number = 2, colour = "H_2" },
                            new Card(){ number = 3, colour = "H_3" },
@@ -87,8 +69,24 @@ namespace kortlek21
                            new Card(){ number = 12, colour = "C_Q" },
                            new Card(){ number = 13, colour = "C_K" },
 
-                        };
+            };
 
+            int start = 1;
+            do
+            {
+
+                //Switch case meny.
+
+                char ch;
+                Console.WriteLine("\nSelect a Program \n A , B , C \nPress Q then enter to exit");
+                ch = Convert.ToChar(Console.ReadLine());
+
+                switch (char.ToLower(ch))
+                {
+                    case 'a':
+
+
+                        Console.WriteLine("\nHere is your deck in a random order\n");
 
                         //Shuffles the list.
                         
@@ -122,12 +120,43 @@ namespace kortlek21
                         break;
 
                     case 'b':
-                        // note to future johan
-                        //create a dictonary to assign vaules to the list 
-                        //split korten, [s_5] = [s_/5] , ge sedan 5 = int 5.
-                        // något åt det hållet, from johan in da past
+                        Console.WriteLine("\nWould you like a card?\nYes  /  NO \n");
+                        String answer = Console.ReadLine();
+                        String answerLow = answer.ToLower();
+
+                        if(answerLow == "yes")
+                        {
+
+                            //Shuffles the list.
+
+                            RNGCryptoServiceProvider providerTwo = new RNGCryptoServiceProvider();
+                            int m = cardDeck.Count;
+                            while (m > 1)
+                            {
+                                byte[] box = new byte[1];
+                                do providerTwo.GetBytes(box);
+                                while (!(box[0] < m * (Byte.MaxValue / m)));
+                                int k = (box[0] % m);
+                                m--;
+                                Card value = cardDeck[k];
+                                cardDeck[k] = cardDeck[m];
+                                cardDeck[m] = value;
+                            }
+
+                            
+
+                            Console.WriteLine("You pulled {0} and your score is {1}\n" ,cardDeck[0].colour, cardDeck[0].number);
 
 
+                        }
+                        else if(answerLow == "no")
+                        {
+                            Console.WriteLine("\nOkay");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nCan not compute\n Please try again\n");
+                        }
 
 
 
@@ -137,6 +166,103 @@ namespace kortlek21
 
                     case 'c':
 
+                        //gör en for sats som gör så att man kan dra kort om och om eller en while sats
+                        Console.WriteLine("\nWelcome to BlackJack ala Drohan\nStart playing\n YES  /  NO \n");
+                        String answerTwo = Console.ReadLine();
+                        String answerTwoLow = answerTwo.ToLower();
+
+                        if (answerTwoLow == "yes")
+                        {
+                            //shuffle list (cardDeck)
+                            RNGCryptoServiceProvider providerTwo = new RNGCryptoServiceProvider();
+                            int v = cardDeck.Count;
+                            while (v > 1)
+                                {
+                                    byte[] box = new byte[1];
+                                    do providerTwo.GetBytes(box);
+                                    while (!(box[0] < v * (Byte.MaxValue / v)));
+                                    int k = (box[0] % v);
+                                    v--;
+                                    Card value = cardDeck[k];
+                                    cardDeck[k] = cardDeck[v];
+                                    cardDeck[v] = value;
+                                }
+
+                            Console.WriteLine("\nDraw a card?\n YES  /  NO\n");
+                            String answerThree = Console.ReadLine();
+                            String answerThreeLow = answerTwo.ToLower();
+                            int points = 0;
+                            int i = 0;
+
+                            if (answerThreeLow == "yes")
+                                {
+                                    //int count = 1;
+
+                                    while(points < 21)
+                                    {
+
+                                   
+                                        points = points + cardDeck[i].number;
+                                    
+                                        Console.WriteLine("\nYou pulled {0} your current score is {1}", cardDeck[i].colour, points);                                       
+                                        Console.WriteLine("Draw again?\n YES  /  NO\n");                                                                                         
+
+                                        String answerFour = Console.ReadLine();
+                                        String answerFourLow = answerFour.ToLower();
+                                        i++;   
+
+
+
+                                        if (answerFourLow == "yes")
+                                        {
+                                            //count = 1;
+                                        }
+                                        else if (answerFourLow == "no")
+                                        {
+                                            Console.WriteLine("\nThanks for playing\n");
+                                        //count = 0;
+                                        break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("\nSorry can not compute\n");
+                                        //count = 0;
+                                        break;
+                                        }
+
+                                        if (points > 21)
+                                        {
+                                            Console.WriteLine("\nBad luck you got {0} better luck next time\n", points);
+                                        //count = 0;
+                                        break;
+                                        }
+                                        else if (points < 21)
+                                        {
+                                            //count = 1;
+                                        }
+                                        else if (points == 21)
+                                        {
+                                            Console.WriteLine("\nCONGRATZ YOU GOT 21\nYOU WON\nClaim your starfighter (=0=)");
+                                        //count = 1;
+                                        break;
+                                        }
+
+                                    }
+
+                                
+
+                                }
+                        }
+
+                        else if (answerTwoLow == "no")
+                        {
+                            Console.WriteLine("\nOkay");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("\nCan not compute\nPlease try again\n");
+                        }
 
                         start = 1;
                         break;
@@ -157,6 +283,8 @@ namespace kortlek21
 
         }
     }
+
+    // decide what a card should contain.
     public class Card
     {
         public int number { get; set; }
